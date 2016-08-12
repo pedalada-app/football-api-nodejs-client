59f0f94c4c56a937c20c8fd81057d0777193ebf7
Danny <dannyb7878@gmail.com>
2016-08-12 11:25:30 +0300

    teams

--- src/main/index.js
+++ src/main/index.js
@@ -4,24 +4,20 @@ var request = require('request-promise');
 
 var BASE_URL = "https://api.football-data.org/v1";
 
-var options = {
-	method: 'POST',
-	uri: 'http://posttestserver.com/post.php',
-	headers: {}
-};
-
 var getOptions = function (apiKey, resource) {
 
 	return {
 		method: 'GET',
 		url: BASE_URL + resource,
 		headers: {
-			'X-Auth-Token': apiKey
+			'X-Auth-Token': apiKey,
+			'X-Response-Control': "minified"
 		},
 		resolveWithFullResponse: true
 	};
 
 };
+
 
 class FootballApiClient {
 
@@ -31,20 +27,63 @@ class FootballApiClient {
 
 	}
 
-	getCompetitionById(id) {
+	getTeam(id) {
 
-		let competitionResource = "/competitions/" + id;
-
-		return request(getOptions(this.apiKey, competitionResource))
-			.then(function (result) {
-
-				console.log(result.body);
-				console.log(result.headers);
-
-			});
+		return new TeamClientWrapper(this.apiKey, id);
 
 	}
 
+}
+
+function mergeResources(response) {
+
+	return {
+		data: response.body,
+		metadata: {
+			requestCount: response.headers['x-requests-available'],
+			requestCountReset: response.headers['x-requestcounter-reset']
+		}
+	};
+
+
+}
+
+function makeRequest(apiKey, apiResource, options) {
+
+	return request(getOptions(apiKey, apiResource, options))
+		.then(mergeResources);
+}
+
+class TeamClientWrapper {
+
+	constructor(apiKey, id) {
+		this.apiKey = apiKey;
+		this.id = id;
+	}
+
+	getInfo() {
+
+		let apiResource = "/teams/" + this.id;
+
+		return makeRequest(this.apiKey, apiResource, this.apiKey);
+
+	}
+
+	getFixtures(options) {
+
+		let apiResource = "/teams/" + this.id + "/fixtures";
+
+		return makeRequest(this.apiKey, apiResource, options);
+
+	}
+
+	getPlayers() {
+
+		let apiResource = "/teams/" + this.id + "/players";
+
+		return makeRequest(this.apiKey, apiResource);
+
+	}
 
 }
 
--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,32 @@
 var req = require('../main/index')('7496a844b5b8451fb53526e5d7c650d3');
 
-req.getCompetitionById(424)
-   .catch(function (err) {
+//req.getCompetitionById(424)
+//   .catch(function (err) {
+//
+//	   console.error(err);
+//
+//   });
 
-	   console.error(err);
+req.getTeam(57)
+   .getInfo()
+   .then(function (res) {
+
+	   console.log(res);
+
+   });
+
+req.getTeam(57)
+   .getPlayers()
+   .then(function (res) {
+
+	   console.log(res);
+
+   });
+
+req.getTeam(57)
+   .getFixtures()
+   .then(function (res) {
+
+	   console.log(res);
 
    });
