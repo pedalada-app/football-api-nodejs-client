4c56b6bcb913faa623aac739917dde64970b5330
vilnacorp <vilnacorp@gmail.com>
2016-08-12 11:23:09 +0300

    competition client

--- src/main/index.js
+++ src/main/index.js
@@ -10,7 +10,9 @@ var options = {
 	headers: {}
 };
 
-var getOptions = function (apiKey, resource) {
+var getOptions = function (apiKey, resource, queryParams) {
+
+	queryParams = queryParams || {};
 
 	return {
 		method: 'GET',
@@ -18,7 +20,8 @@ var getOptions = function (apiKey, resource) {
 		headers: {
 			'X-Auth-Token': apiKey
 		},
-		resolveWithFullResponse: true
+		resolveWithFullResponse: true,
+		qs: queryParams
 	};
 
 };
@@ -31,20 +34,77 @@ class FootballApiClient {
 
 	}
 
-	getCompetitionById(id) {
+	getCompetitions(season) {
 
-		let competitionResource = "/competitions/" + id;
+		let queryParams = season ? {season: season} : undefined;
+		let competitionResource = "/competitions";
 
-		return request(getOptions(this.apiKey, competitionResource))
-			.then(function (result) {
-
-				console.log(result.body);
-				console.log(result.headers);
-
-			});
+		return request(getOptions(this.apiKey, competitionResource, queryParams))
+			.then(mergeResources);
 
 	}
 
+	getCompetitionById(compId) {
+
+		return new Competition(this.apiKey, compId);
+
+	}
+
+	getFixures(options) {
+
+		let fixturesResource = "/fixtures";
+	}
+
+	getFixureById(fixtureId, head2head) {
+
+	}
+
+}
+
+class Competition {
+
+	constructor(apiKey, id) {
+
+		this.id = id;
+		this.apiKey = apiKey;
+
+	}
+
+	getInfo() {
+
+		let competitionResource = "/competitions/" + this.id;
+
+		return request(getOptions(this.apiKey, competitionResource))
+			.then(mergeResources);
+	}
+
+	getTeams() {
+
+		let competitionResource = "/competitions/" + this.id + "/teams";
+
+		return request(getOptions(this.apiKey, competitionResource))
+			.then(mergeResources);
+	}
+
+	getTable(matchday) {
+
+		let queryParams = matchday ? {matchday: matchday} : undefined;
+		let competitionResource = "/competitions/" + this.id + "/leagueTable";
+
+		return request(getOptions(this.apiKey, competitionResource, queryParams))
+			.then(mergeResources);
+
+	}
+
+	getFixtures(options) {
+
+		let queryParams = options ? options : undefined;
+		let competitionResource = "/competitions/" + this.id + "/fixtures";
+
+		return request(getOptions(this.apiKey, competitionResource, queryParams))
+			.then(mergeResources);
+
+	}
 
 }
 
